5f8d2a891acfe3d56129f4768469506fbeee1d70
SergioAG <sergdalag@gmail.com>
2018-07-23 18:30:54 +0200

    Repaso

--- templates/404.php
+++ templates/404.php
@@ -1,6 +1,5 @@
 <div id="body-container" class="container">  
-<div class="container-fluid img_404">
-    <img id="img_404" src="<?php echo $carpeta_fotos?>/404.gif" alt="img">
-
-</div>
+    <div class="container-fluid img_404">
+        <img id="img_404" src="<?php echo $carpeta_fotos?>/404.gif" alt="img">
+    </div>
 </div>
